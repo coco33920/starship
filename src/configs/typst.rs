050c46be4c25f4e506ab2944eea3fb68fca0801e
Charlotte Thomas <contact@nwa2coco.fr>
2023-10-26 21:51:36 +0200

    change char for typst

--- src/configs/typst.rs
+++ src/configs/typst.rs
@@ -23,7 +23,7 @@ impl<'a> Default for TypstConfig<'a> {
         TypstConfig {
             format: "via [$symbol($version )]($style)",
             version_format: "v${raw}",
-            symbol: "Ұ̈ ",
+            symbol: "\u{0074}\u{0308} ",
             style: "bright-blue",
             disabled: false,
             detect_extensions: vec!["typ"],
